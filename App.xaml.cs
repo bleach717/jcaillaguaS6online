4e6034c4b95a102c91524d8c3c70f9e6076e6ee9
Uisrael-Caillagua <e1724168784@uisrael.edu.ec>
2025-11-16 14:25:52 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace jcaillaguaS6online
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new Views.vistaEstudiante());
+        }
+    }
+}
---
+++ Views/vistaEstudiante.xaml.cs
@@ -0,0 +1,26 @@
+
+using jcaillaguaS6online.Models;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+
+namespace jcaillaguaS6online.Views;
+
+public partial class vistaEstudiante : ContentPage
+{
+	private const string Url = "http://192.168.2.19/wsestudiante/restEstudiante.php";
+	private readonly HttpClient cliente = new HttpClient();
+	private ObservableCollection<Estudiante> _estudiante;
+
+	public async void Mostrar()
+	{
+		var contet = await cliente.GetStringAsync(Url);
+		List<Estudiante> mostarEstudiante = JsonConvert.DeserializeObject<List<Estudiante>>(contet);
+		_estudiante = new ObservableCollection<Estudiante>(mostarEstudiante);
+        listaEstudiantes.ItemsSource = _estudiante;
+	}
+    public vistaEstudiante()
+	{
+		InitializeComponent();
+		Mostrar();
+	}
+}

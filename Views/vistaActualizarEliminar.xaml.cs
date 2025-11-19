dd108ab23ef134f0e66f241be582a2ec6740e760
Uisrael-Caillagua <e1724168784@uisrael.edu.ec>
2025-11-18 21:26:52 -0500

    Delete y Update

--- Views/vistaActualizarEliminar.xaml.cs
+++ Views/vistaActualizarEliminar.xaml.cs
@@ -1,4 +1,5 @@
 using jcaillaguaS6online.Models;
+using System.Net;
 
 namespace jcaillaguaS6online.Views;
 
@@ -15,11 +16,43 @@ public partial class vistaActualizarEliminar : ContentPage
 
     private void btnActaulizar_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            string url = $"http://192.168.2.19/wsestudiante/restEstudiante.php" +
+                     $"?codigo={txtCodigo.Text}" +
+                     $"&nombre={txtNombre.Text}" +
+                     $"&apellido={txtApellido.Text}" +
+                     $"&edad={txtEdad.Text}";
 
+            WebClient cliente = new WebClient();
+            cliente.UploadString(url, "PUT", "");
+
+            DisplayAlert("Alert", "Actualización correcta", "OK");
+            Navigation.PushAsync(new vistaEstudiante());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex);
+        }
     }
 
     private void brnEliminar_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            string codigo = txtCodigo.Text;
 
+            string url = $"http://192.168.2.19/wsestudiante/restEstudiante.php?codigo={codigo}";
+
+            WebClient cliente = new WebClient();
+            cliente.UploadString(url, "DELETE", "");
+
+            DisplayAlert("Alert", "Registro eliminado correctamente", "OK");
+            Navigation.PushAsync(new vistaEstudiante());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Error", ex.ToString(), "OK");
+        }
     }
 }
